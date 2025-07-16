be94a62d86051158fd8ac33991688a2b8e5b758f
Chee Meng Hong <cheemeng.hong@alpropharmacy.com>
2025-07-16 14:03:27 +0800

    feat: vite config

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,24 @@ import laravel from 'laravel-vite-plugin'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+	base: '/staging/public/',
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+            buildDirectory: 'build',
         }),
-        tailwindcss()
     ],
+    build: {
+        outDir: 'public/build',
+        emptyOutDir: true,
+        manifest: true,
+        rollupOptions: {
+            output: {
+                entryFileNames: 'assets/[name].[hash].js',
+                chunkFileNames: 'assets/[name].[hash].js',
+                assetFileNames: 'assets/[name].[hash].[ext]'
+            }
+        }
+    }
 })
