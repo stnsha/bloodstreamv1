a94c9e1582e213f16257f16b30b10916dca2d777
Siti Anasuha <anasuharosli@gmail.com>
2025-07-22 10:57:06 +0800

    feat: new migration files - panel category and profile, result library

---
+++ database/migrations/2025_07_22_104530_create_result_libraries_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('result_libraries', function (Blueprint $table) {
+            $table->id();
+            $table->string('column_name');
+            $table->string('code')->nullable();
+            $table->string('description')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('result_libraries');
+    }
+};
