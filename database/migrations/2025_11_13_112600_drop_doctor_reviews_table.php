53be787c0aaf0523e0cb05eee60e12cf1e7e36ad
Siti Anasuha <anasuharosli@gmail.com>
2025-11-13 12:38:24 +0800

    feat: drop doc review table

---
+++ database/migrations/2025_11_13_112600_drop_doctor_reviews_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::disableForeignKeyConstraints();
+        Schema::dropIfExists('doctor_reviews');
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::create('doctor_reviews', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('test_result_id');
+            $table->json('compiled_results');
+            $table->longText('review');
+            $table->boolean('is_sync')->default(false);
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign('test_result_id')->references('id')->on('test_results')->onDelete('cascade');
+        });
+    }
+};
